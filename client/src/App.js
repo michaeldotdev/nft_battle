bba6bfb7f9d5ba6397e3faf6fdb8f3f6c25bee4e
Michael Su <michaelsu.jobs@gmail.com>
2021-10-30 21:45:09 -0700

    Update App.js with wallet auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -7,6 +7,60 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+    const [currentAccount, setCurrentAccount] = useState(null);
+
+    // Actions
+    const checkIfWalletIsConnected = async () => {
+      try {
+        const { ethereum } = window;
+  
+        if (!ethereum) {
+          console.log('Make sure you have MetaMask!');
+          return;
+        } else {
+          console.log('We have the ethereum object', ethereum);
+        }
+  
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+  
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    const connectWalletAction = async () => {
+      try {
+        const { ethereum } = window;
+  
+        if (!ethereum) {
+          alert('Get MetaMask!');
+          return;
+        }
+  
+        const accounts = await ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+  
+        console.log('Connected', accounts[0]);
+        setCurrentAccount(accounts[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    useEffect(() => {
+      checkIfWalletIsConnected();
+    }, []);
+
+
+
   return (
     <div className="App">
       <div className="container">
@@ -16,6 +70,12 @@ const App = () => {
           <p className="sub-text">Use with Testnet only!</p>
           <p className="sub-text">Images are from Riot Games</p>
           <div className="connect-wallet-container">
+          <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started
+            </button>
           </div>
         </div>
         <div className="footer-container">
