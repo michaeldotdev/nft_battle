8b8f8dbcb2b9089716d383d50b11054b4b8ec399
Michael Su <michaelsu.jobs@gmail.com>
2021-10-30 21:30:12 -0700

    Add client starter files

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
+import './App.css';
+
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ NFT Battler! ⚔️</p>
+          <p className="sub-text">Time to battle it out!</p>
+          <p className="sub-text">Use with Testnet only!</p>
+          <p className="sub-text">Images are from Riot Games</p>
+          <div className="connect-wallet-container">
+          </div>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
