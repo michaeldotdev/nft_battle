d252f9f22258346f417fcf303d70dac9b59abd0b
Michael Su <michaelsu.jobs@gmail.com>
2021-10-30 22:33:21 -0700

    Implement logic, allowing user to select and mint champion

---
+++ client/src/Components/SelectCharacter/index.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import './SelectCharacter.css';
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import myNFTBattler from '../../utils/MyNFTBattler.json';
+
+/*
+ * Don't worry about setCharacterNFT just yet, we will talk about it soon!
+ */
+const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+
+  const mintCharacterNFTAction = (characterId) => async () => {
+    try {
+      if (gameContract) {
+        console.log('Minting character in progress...');
+        const mintTxn = await gameContract.mintChampionNFT(characterId);
+        await mintTxn.wait();
+        console.log('mintTxn:', mintTxn);
+      }
+    } catch (error) {
+      console.warn('MintCharacterAction Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    const { ethereum } = window;
+  
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myNFTBattler.abi,
+        signer
+      );
+      setGameContract(gameContract);
+    } else {
+      console.log('Ethereum object not found');
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log('Getting contract characters to mint');
+  
+        const charactersTxn = await gameContract.getAllDefaultCharacters();
+        console.log('charactersTxn:', charactersTxn);
+  
+        const characters = charactersTxn.map((characterData) =>
+          transformCharacterData(characterData)
+        );
+  
+        setCharacters(characters);
+      } catch (error) {
+        console.error('Something went wrong fetching characters:', error);
+      }
+    };
+  
+    /*
+     * Add a callback method that will fire when this event is received
+     */
+    const onCharacterMint = async (sender, tokenId, characterIndex) => {
+      console.log(
+        `CharacterNFTMinted - sender: ${sender} tokenId: ${tokenId.toNumber()} characterIndex: ${characterIndex.toNumber()}`
+      );
+
+      if (gameContract) {
+        const characterNFT = await gameContract.checkIfUserHasNFT();
+        console.log('CharacterNFT: ', characterNFT);
+        setCharacterNFT(transformCharacterData(characterNFT));
+      }
+    };
+  
+    if (gameContract) {
+      getCharacters();
+      gameContract.on('CharacterNFTMinted', onCharacterMint);
+    }
+  
+    return () => {
+      if (gameContract) {
+        gameContract.off('CharacterNFTMinted', onCharacterMint);
+      }
+    };
+  }, [gameContract, setCharacterNFT]);
+
+  const renderCharacters = () =>
+  characters.map((character, index) => (
+    <div className="character-item" key={character.name}>
+      <div className="name-container">
+        <p>{character.name}</p>
+      </div>
+      <img src={character.imageURI} alt={character.name} />
+      <button
+        type="button"
+        className="character-mint-button"
+        onClick={mintCharacterNFTAction(index)}
+      >{`Mint ${character.name}`}</button>
+    </div>
+  ));
+
+
+  return (
+    <div className="select-character-container">
+      <h2>Mint Your Champion. Choose wisely.</h2>
+      {characters.length > 0 && (
+      <div className="character-grid">{renderCharacters()}</div>
+    )}
+    </div>
+  );
+};
+
+export default SelectCharacter;
--- client/src/constants.js
+++ client/src/constants.js
@@ -4,8 +4,8 @@ const transformCharacterData = (characterData) => {
   return {
     name: characterData.name,
     imageURI: characterData.imageURI,
-    hp: characterData.hp.toNumber(),
-    maxHp: characterData.maxHp.toNumber(),
+    currentHP: characterData.currentHP.toNumber(),
+    maxHP: characterData.maxHP.toNumber(),
     attackDamage: characterData.attackDamage.toNumber(),
   };
 };
